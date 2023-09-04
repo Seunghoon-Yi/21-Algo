0b8d7eac8f159ea0ee6a879db3e4a874e6c46503
Seunghoon-Yi <jaguar6182@snu.ac.kr>
2023-09-04 09:38:28 +0900

    added DFS problems

--- Algorithms/Searching_Graphs/BOJ_24481.py
+++ Algorithms/Searching_Graphs/BOJ_24481.py
@@ -7,6 +7,7 @@ n, m, v = map(int, input().split())
 graph = [[] for _ in range(n+1)]
 visited = [False] * (n+1)
 result = [0]*(n+1)
+depth = [-1]*(n+1)
 
 # initialization
 for _ in range(m):
@@ -16,20 +17,25 @@ for _ in range(m):
     graph[to_].append(from_)
 
 for i in range(1, n+1):
-    #graph[i].sort()                 # 24479번
-    graph[i].sort(reverse = True)   # 24480번
+    graph[i].sort()                 
 
 # DFS algorithm
 cnt = 1
-def dfs(graph, start_node):
+def dfs(graph, start_node, curr_depth = 0):
     global cnt
     visited[start_node] =True
     result[start_node] = cnt
     cnt += 1
+    depth[start_node] = curr_depth
 
     for adj_node in graph[start_node]:
         if not visited[adj_node]:
-            dfs(graph, adj_node)
+            dfs(graph, adj_node, curr_depth+1)
 
 dfs(graph, v)
-print(*result[1:], sep='\n')
+# print(*depth[1:], sep='\n') # 24480, 24481번
+# 24482번
+sum = 0
+for i in range(1, n+1):
+    sum+=result[i] * depth[i]
+print(sum)
